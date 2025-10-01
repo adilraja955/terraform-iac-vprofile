5d067fa5d763b9d4a13c32f32f94dbe5da9f1058
adilraja955 <maadil2020@outlook.com>
2025-10-02 03:10:42 +0500

    local changed to locals

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -2,7 +2,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.0"
 
-  cluster_name    = local.cluster_name
+  cluster_name    = locals.cluster_name
   cluster_version = "1.33"
 
   vpc_id                         = module.vpc.vpc_id
