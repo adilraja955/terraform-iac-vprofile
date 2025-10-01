ad5fc1111d7048582dedf5d2c87bd362904d3040
adilraja955 <maadil2020@outlook.com>
2025-10-02 04:29:59 +0500

    change cluster version to kubernetes version 1.33

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -2,8 +2,8 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.0"
 
-  cluster_name    = local.cluster_name
-  cluster_version = "1.27"
+  cluster_name    = locals.cluster_name
+  kubernetes_version = "1.33"
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
