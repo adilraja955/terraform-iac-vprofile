4d67f0d9fea592dd3b27a67cc76867e001a83544
adilraja955 <maadil2020@outlook.com>
2025-10-02 05:20:10 +0500

    Eks changes

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -7,17 +7,14 @@ module "eks" {
 
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
+  endpoint_public_access = true
 
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-
-  }
 
   eks_managed_node_groups = {
     one = {
       name = "node-group-1"
-
+      
+      ami_type = "AL2_x86_64"
       instance_types = ["t3.small"]
 
       min_size     = 1
@@ -28,6 +25,7 @@ module "eks" {
     two = {
       name = "node-group-2"
 
+      ami_type = "AL2_x86_64"
       instance_types = ["t3.small"]
 
       min_size     = 1
