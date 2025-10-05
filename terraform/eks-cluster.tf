649d42452339efe776acd77d5b1849192d995488
adilraja955 <maadil2020@outlook.com>
2025-10-05 23:23:01 +0500

    Eks module version 21.0 some changes

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -11,8 +11,7 @@ module "eks" {
 
 
   eks_managed_node_groups = {
-    one = {
-      name = "node-group-1"
+    example = {
       
       ami_type = "AL2_x86_64"
       instance_types = ["t3.small"]
@@ -22,15 +21,5 @@ module "eks" {
       desired_size = 2
     }
 
-    two = {
-      name = "node-group-2"
-
-      ami_type = "AL2_x86_64"
-      instance_types = ["t3.small"]
-
-      min_size     = 1
-      max_size     = 2
-      desired_size = 1
-    }
   }
 }
